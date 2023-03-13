00ff3af6135fbf5290443838be6462d6a50a82db
sagongjieun <gonii0821@naver.com>
2023-03-13 17:56:22 +0900

    🐾[FE:feat] 글로벌 스타일 font 크기 세팅

--- FE/ghem/src/GlobalStyles.tsx
+++ FE/ghem/src/GlobalStyles.tsx
@@ -7,12 +7,41 @@ function GlobalStyles() {
 const globalStyles = css`
   @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css");
 
+  h1 {
+    font-size: 2.125rem;
+  }
+
+  h2 {
+    font-size: 1.875rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+  }
+
+  h4 {
+    font-size: 1.25rem;
+  }
+
+  h5 {
+    font-size: 1.125rem;
+  }
+
+  h6,
+  p {
+    font-size: 1rem;
+  }
+
   * {
     margin: 0;
     padding: 0;
     text-decoration: none;
     box-sizing: border-box;
     font-family: "Pretendard";
+  }
+
+  html {
+    font-size: 18px;
   }
 
   ::-webkit-scrollbar {
