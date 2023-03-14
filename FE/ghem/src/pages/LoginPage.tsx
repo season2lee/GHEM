56cafa58ec5f491f5f48d2290fc82c263a8f7972
sagongjieun <gonii0821@naver.com>
2023-03-14 10:41:07 +0900

    🐾[FE:feat] 로그인페이지 배경 레이아웃 구현

--- FE/ghem/src/pages/LoginPage.tsx
+++ FE/ghem/src/pages/LoginPage.tsx
@@ -1,7 +1,26 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
+import { css } from "@emotion/react";
+import loginBackground from "../assets/image/loginBackground.jpg";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div css={wrapper}>
+      <div css={linearWrapper}></div>
+    </div>
+  );
 }
+
+const wrapper = css`
+  width: 100vw;
+  height: 100vh;
+  background-color: #292233;
+`;
+
+const linearWrapper = css`
+  width: 100vw;
+  height: 600px;
+  background-image: linear-gradient(360deg, #292233 0%, rgba(41, 34, 51, 0) 169.72%), url(${loginBackground});
+`;
 
 export default LoginPage;
