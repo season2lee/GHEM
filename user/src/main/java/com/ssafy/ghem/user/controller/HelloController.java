c089c85e87a158b073a3f8adf4f876d933076340
SeonyongPark <tony3337@naver.com>
2023-03-14 16:29:17 +0900

    [be:test] 테스트

---
+++ user/src/main/java/com/ssafy/ghem/user/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.ssafy.ghem.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    ResponseEntity<?> sayHello(){
+        Map<String, String> map = new HashMap<>();
+        map.put("say", "hello");
+
+        return new ResponseEntity<Map>(map, HttpStatus.OK);
+    }
+
+}
