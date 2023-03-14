ad32bee883a261b273a0153b8366866f632c64d2
Lee Sijun <season2lee@gmail.com>
2023-03-14 14:32:01 +0900

    💎[FE:feat] navbar 크기, 네비게이션 설정

--- FE/ghem/src/components/common/Navbar.tsx
+++ FE/ghem/src/components/common/Navbar.tsx
@@ -1,7 +1,37 @@
+import { css } from "@emotion/react";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div css={navbar}>
+      <NavLink to="/">Ghem</NavLink>
+      <div>
+        <NavLink to="/main">main</NavLink>
+        <NavLink to="/login">login</NavLink>
+      </div>
+    </div>
+  );
 }
+
+const navbar = css`
+  height: 70px;
+  background-color: #292233;
+  display: flex;
+  justify-content: space-between;
+  /* position: fixed;
+  top: 0; */
+  width: 100%;
+  div {
+    padding: 1rem;
+  }
+  a {
+    color: #f1eff4;
+    padding: 0rem 1rem;
+  }
+  > a {
+    padding: 1rem;
+  }
+`;
 
 export default Navbar;
---
+++ FE/ghem/src/components/main/friend/FriendList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FriendListItem from "./FriendListItem";
+
+function FriendList() {
+  return (
+    <div>
+      FriendList
+      <FriendListItem />
+    </div>
+  );
+}
+
+export default FriendList;
