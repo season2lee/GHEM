ff21768092a11fa2976bb7d6aafc487d3d3ccce5
sagongjieun <gonii0821@naver.com>
2023-03-16 17:41:44 +0900

    🐾[FE:modify] 마이프로필 반응형을 위해 크기를 %로 변경

--- FE/ghem/src/components/profile/ProfileMenu.tsx
+++ FE/ghem/src/components/profile/ProfileMenu.tsx
@@ -37,7 +37,7 @@ function ProfileMenu({ text, onClickOpenHiddenMenu }: ProfileMenuProps) {
 }
 
 const profileMenuWrapper = css`
-  width: 250px;
+  width: 100%;
   height: 50px;
   background: #463b58;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
