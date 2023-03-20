1ce700f549deb5308a2be53b7f344b8dc6d575e3
SeonyongPark <tony3337@naver.com>
2023-03-20 12:29:47 +0900

    [be:init] init

--- user/src/main/java/com/ssafy/ghem/user/controller/HelloController.java
+++ user/src/main/java/com/ssafy/ghem/user/controller/HelloController.java
@@ -22,6 +22,7 @@ public class HelloController {
         map.put("3", "3");
         map.put("4", "4");
         map.put("5", "5");
+        map.put("6", "6");
 
         return new ResponseEntity<Map>(map, HttpStatus.OK);
     }
