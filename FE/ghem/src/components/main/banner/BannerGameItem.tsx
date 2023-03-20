3f4087111464a782cc67c7418f1d3aadb8a7a8e3
Lee Sijun <season2lee@gmail.com>
2023-03-20 15:04:49 +0900

    💎[FE:feat] 가로 스크롤 자동 움직임 + 드래그 드랍

--- FE/ghem/src/components/main/banner/BannerGameItem.tsx
+++ FE/ghem/src/components/main/banner/BannerGameItem.tsx
@@ -1,16 +1,29 @@
 import { css } from "@emotion/react";
 import React from "react";
+import { useNavigate } from "react-router";
 import BannerGameItemDetail from "./BannerGameItemDetail";
 
-function BannerGameItem() {
+type BannerGameItemProps = {
+  appId: number;
+  title: string;
+};
+
+function BannerGameItem(props: BannerGameItemProps) {
+  const navigator = useNavigate();
+  const toDetail = () => {
+    navigator(`../detail/${props.appId}`);
+  };
+
   return (
     <div>
       <div css={backImgDiv}>
         <div css={blurDiv}>
           <div css={flexDiv}>
             <img
-              src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${367520}/hero_capsule.jpg`}
-              alt=""
+              src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${props.appId}/hero_capsule.jpg`}
+              // src={`https://cdn.akamai.steamstatic.com/steam/apps/${props.appId}/capsule_616x353.jpg`}
+              alt={`${props.title}`}
+              onClick={toDetail}
             />
             <div>게임 설명 들어갈 건데 그렇게만 알고 있으면 됨</div>
           </div>
