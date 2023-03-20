b9f805bcc4b1a71caeabf33cae67cd7d855871c3
김민지 <moo_miz@naver.com>
2023-03-20 17:30:13 +0900

    Revert "Merge branch 'develop_fe_minji_feat_welcome#2' into 'main'"
    This reverts merge request !42

--- FE/ghem/src/components/main/discount/DiscountGameTag.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function DiscountGameTag() {
-  return <div>DiscountGameTag</div>;
-}
-
-export default DiscountGameTag;
--- FE/ghem/src/components/main/friend/FriendListItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function FriendListItem() {
-  return <div>FriendListItem</div>;
-}
-
-export default FriendListItem;
