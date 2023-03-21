872a183848b9de04dd34d6c1b0d960f715bdca74
sagongjieun <gonii0821@naver.com>
2023-03-21 10:52:00 +0900

    🐾[FE:modify] 반응형 mixin 크기 수정

--- FE/ghem/src/Mixin.ts
+++ FE/ghem/src/Mixin.ts
@@ -1,9 +1,23 @@
-const breakpoints = [767, 1400];
+const breakpoints = [479, 767, 1023];
 
+// 임시 모바일 최대 크기 = 767px
 export const mobile = `
-  @media (max-width: ${breakpoints[0]}px)
-`;
-
-export const desktop = `
   @media (max-width: ${breakpoints[1]}px)
 `;
+
+// 모바일
+// export const mobile = `
+//   @media (max-width: ${breakpoints[0]}px)
+// `;
+
+// 태블릿 가로
+// export const tabletH = `
+//   @media (max-width: ${breakpoints[1]}px)
+// `;
+
+// 태블릿 세로
+// export const tabletV = `
+//   @media (max-width: ${breakpoints[2]}px)
+// `;
+
+// 1024 이상의 크기는 데스크톱
