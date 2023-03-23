34721a8611d84441627ab5502cd2b8aa25892993
Lee Sijun <season2lee@gmail.com>
2023-03-23 11:20:24 +0900

    💎[FE:feat] neon text

--- FE/ghem/src/components/main/Discount.tsx
+++ FE/ghem/src/components/main/Discount.tsx
@@ -67,7 +67,7 @@ function Discount(props: DiscountProps) {
   return (
     <div css={discountDiv}>
       <span>
-        <b>IN DISCOUNT</b>
+        <b css={discountText}>IN DISCOUNT</b>
       </span>
       <CommonGameList
         gameType="discount"
@@ -92,6 +92,12 @@ const discountDiv = css`
   padding: 1rem 0rem 1rem 0rem;
   background-color: #352c42;
   border-radius: 30px;
+`;
+
+const discountText = css`
+  color: #fff;
+  text-shadow: 0 0 1px #fff, 0 0 2px #fff, 0 0 4px #fff, 0 0 7px #f6b4ff,
+    0 0 10px #f1c1ff, 0 0 15px #ffd8f8, 0 0 18px #eb68ff, 0 0 23px #ffa9cb;
 `;
 
 export default Discount;
--- FE/ghem/src/components/main/banner/BannerGameList.tsx
+++ FE/ghem/src/components/main/banner/BannerGameList.tsx
@@ -1,4 +1,4 @@
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
 import React from "react";
 import BannerGameItem from "./BannerGameItem";
 
@@ -7,11 +7,15 @@ function BannerGameList() {
     <div>
       <div css={recommendForU}>
         <span>
-          <b>WHAT FOR YOU</b>
+          <b css={boxStyle}>WHAT FOR YOU</b>
         </span>
         <span>O</span>
       </div>
-      <BannerGameItem appId={367520} title={"hollow"} />
+      <div css={recommendList}>
+        <BannerGameItem appId={367520} title={"hollow"} />
+        <BannerGameItem appId={10} title={"counterscrike"} />
+        <BannerGameItem appId={322330} title={"dontstarv"} />
+      </div>
     </div>
   );
 }
@@ -26,6 +30,34 @@ const recommendForU = css`
   padding: 1rem 4rem 0rem 4rem;
   background-color: #352c42;
   border-radius: 30px 30px 0px 0px;
+`;
+
+const recommendList = css`
+  display: flex;
+  overflow: scroll;
+`;
+
+const floating = keyframes`
+  0%, 18%, 22%, 25%, 53%, 57%, 100% {
+    text-shadow:
+    0 0 1px #fff,
+    0 0 6px #fff,
+    0 0 8px #fff,
+    0 0 10px #f6b4ff,
+    0 0 15px #f1c1ff,
+    0 0 20px #ffd8f8,
+    0 0 30px #dd00ff,
+    0 0 40px #ffa9cb;
+  }
+
+  20%, 24%, 55% {        
+    text-shadow: none;
+  }    
+`;
+
+const boxStyle = css`
+  color: #fff;
+  animation: ${floating} 1.5s infinite alternate; ;
 `;
 
 export default BannerGameList;
