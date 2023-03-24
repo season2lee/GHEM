517b6fc466cb41827277eb99716af7b7f0f1de47
SeonyongPark <tony3337@naver.com>
2023-03-24 09:32:22 +0900

    [be:modify] swagger config 수정

--- user/src/main/java/com/ssafy/ghem/user/config/SwaggerConfiguration.java
+++ user/src/main/java/com/ssafy/ghem/user/config/SwaggerConfiguration.java
@@ -1,9 +1,12 @@
 package com.ssafy.ghem.user.config;
 
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
-import jdk.javadoc.doclet.Doclet;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -26,6 +29,15 @@ import java.util.Set;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addServersItem(new Server().url("/"))
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("SpringShop API").version("V0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
     private Set<String> getConsumeContentTypes() {
         Set<String> consumes = new HashSet<>();
         consumes.add("application/json;charset=UTF-8");
