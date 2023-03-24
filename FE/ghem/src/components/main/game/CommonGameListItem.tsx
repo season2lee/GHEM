957af862117d1a8777445a744e861acbe9b9a9b2
Lee Sijun <season2lee@gmail.com>
2023-03-24 10:47:15 +0900

    💎[FE:feat] 없는 이미지 안보이게 처리

--- FE/ghem/src/components/main/game/CommonGameListItem.tsx
+++ FE/ghem/src/components/main/game/CommonGameListItem.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import DiscountGameDetail from "../discount/DiscountGameDetail";
 import { PageXY } from "@/pages/MainPage";
@@ -24,11 +24,17 @@ type CommonGameListItemProps = {
 
 function CommonGameListItem(props: CommonGameListItemProps) {
   const navigator = useNavigate();
+  const [isError, setIsError] = useState<boolean>(false);
 
   const toDetail = () => {
     if (props.canClick) {
       navigator(`../detail/${props.appid}`);
     }
+  };
+
+  const handleImgError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.style.display = "none";
+    setIsError(true);
   };
 
   return (
@@ -39,7 +45,12 @@ function CommonGameListItem(props: CommonGameListItemProps) {
         props.setColId(props.colId);
         if (!props.isDrag) {
           props.setIsEnter(true);
-          props.setPageXY({ x: e.clientX, y: e.clientY });
+          let x: number = e.clientX;
+          let y: number = e.clientY;
+          // if (e.clientX) {
+
+          // }
+          props.setPageXY({ x, y });
         }
       }}
       onMouseLeave={() => {
@@ -53,6 +64,7 @@ function CommonGameListItem(props: CommonGameListItemProps) {
             css={imgsize}
             src={`https://cdn.cloudflare.steamstatic.com/steam/apps/${props.appid}/header.jpg`}
             alt={`${props.appid}`}
+            onError={handleImgError}
           />
         )}
         {props.imgType === "capsule" && (
@@ -62,7 +74,7 @@ function CommonGameListItem(props: CommonGameListItemProps) {
           />
         )}
       </div>
-      {props.gameType === "discount" && (
+      {props.gameType === "discount" && !isError && (
         <DiscountGameDetail
           discountPercent={props.discountPercent}
           originalPrice={props.originalPrice}
