8c31b657777316a13e76f23649010ecf98dd9691
SeonyongPark <tony3337@naver.com>
2023-03-27 16:47:11 +0900

    [be:modify] content 및 review 기능 수정

--- user/src/main/java/com/ssafy/ghem/user/model/service/ReviewService.java
+++ user/src/main/java/com/ssafy/ghem/user/model/service/ReviewService.java
@@ -6,7 +6,7 @@ import com.ssafy.ghem.user.model.vo.ReviewVO;
 import java.rmi.AlreadyBoundException;
 
 public interface ReviewService {
-    HttpVo doReview(ReviewVO reviewInfo) throws AlreadyBoundException;
+    HttpVo doReview(ReviewVO reviewInfo);
     HttpVo checkReview(ReviewVO reviewInfo);
     HttpVo deleteReview(Long user_game_id);
     HttpVo updateReview(ReviewVO reviewInfo);
