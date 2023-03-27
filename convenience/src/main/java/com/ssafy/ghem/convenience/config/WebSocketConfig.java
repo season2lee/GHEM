b879b82a1c8215ccfcf5913535f0baf649eb4363
SeonyongPark <tony3337@naver.com>
2023-03-27 16:14:37 +0900

    [be:modify] content 및 review 기능 수정

--- convenience/src/main/java/com/ssafy/ghem/convenience/config/WebSocketConfig.java
+++ convenience/src/main/java/com/ssafy/ghem/convenience/config/WebSocketConfig.java
@@ -13,14 +13,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     // 클라이언트가 메시지를 구독할 endpoint를 정의합니다.
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/send");
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
     }
 
     @Override
     // connection을 맺을때 CORS 허용합니다.
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        System.out.println(System.currentTimeMillis());
-        registry.addEndpoint("/chat")
+        registry.addEndpoint("/ws")
                 // 스프링 5.3, 스프링부트 2.4 버전 부터 allowCredentials이 true인 경우 setAllowedOrigins 메서드에서
                 // 와일드 카드 `'*'`을 사용하실 수 없습니다.
                 //  	.setAllowedOrigins("*")
---
+++ convenience/src/main/java/com/ssafy/ghem/convenience/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.ssafy.ghem.convenience.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final SimpMessagingTemplate template;
+
+    @MessageMapping("/message")
+    public void sendMessage(Message message) {
+        template.convertAndSend("/sub/"+message, message);
+    }
+}
