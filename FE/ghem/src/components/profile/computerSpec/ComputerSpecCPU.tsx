592d6c6971a6c675fffca269c6ed3130635652d9
사공지은 <gonii0821@naver.com>
2023-03-27 23:54:18 +0900

    🐾[FE:feat] 컴퓨터 사양 selectBox props 설정

--- FE/ghem/src/components/profile/computerSpec/ComputerSpecCPU.tsx
+++ FE/ghem/src/components/profile/computerSpec/ComputerSpecCPU.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import { css } from "@emotion/react";
 import SelectBox from "./common/SelectBox";
 import { mobile } from "@/util/Mixin";
 
 function ComputerSpecCPU() {
+  const [manufacturer, setManufacturer] = useState<string[]>(["제조사"]);
+  const [series, setSeries] = useState<string[]>(["시리즈"]);
+  const [generation, setGeneration] = useState<string[]>(["세대"]);
+
   return (
     <div css={ComputerSpecWrapper}>
       <h5>CPU</h5>
       <div css={selectBoxWrapper}>
-        <SelectBox />
-        <SelectBox />
-        <SelectBox />
+        <SelectBox optionList={manufacturer} />
+        <SelectBox optionList={series} />
+        <SelectBox optionList={generation} />
       </div>
     </div>
   );
--- FE/ghem/src/components/profile/computerSpec/ComputerSpecGPU.tsx
+++ FE/ghem/src/components/profile/computerSpec/ComputerSpecGPU.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { css } from "@emotion/react";
 import SelectBox from "./common/SelectBox";
 import { mobile } from "@/util/Mixin";
 
 function ComputerSpecGPU() {
+  const [manufacturer, setManufacturer] = useState<string[]>(["제조사"]);
+  const [modelName, setModelName] = useState<string[]>(["모델명"]);
+
   return (
     <div css={ComputerSpecWrapper}>
       <h5>GPU</h5>
       <div css={selectBoxWrapper}>
-        <SelectBox />
-        <SelectBox />
+        <SelectBox optionList={manufacturer} />
+        <SelectBox optionList={modelName} />
       </div>
     </div>
   );
