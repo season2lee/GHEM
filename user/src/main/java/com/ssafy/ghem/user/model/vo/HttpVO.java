f28a624809fab9842b2821fb804ce991c07dd721
이석원 <tjrdnjs67@naver.com>
2023-03-29 22:37:32 +0900

    🌚[be:fix] HttpVO.java 변경 사항 커밋

--- user/src/main/java/com/ssafy/ghem/user/model/vo/HttpVO.java
+++ user/src/main/java/com/ssafy/ghem/user/model/vo/HttpVO.java
@@ -1,2 +1,8 @@
-package com.ssafy.ghem.user.model.vo;public class HttpVO {
+package com.ssafy.ghem.user.model.vo;
+import lombok.*;
+
+@Data
+public class HttpVO {
+    boolean flag;
+    Object data;
 }
