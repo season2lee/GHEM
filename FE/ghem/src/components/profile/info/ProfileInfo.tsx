69e025aecc4100e6394f5ee2c6b4179b450526b5
sagongjieun <gonii0821@naver.com>
2023-03-30 15:24:50 +0900

    🐾[FE:feat] 맞팔 여부에 따라 찜한 목록 보이게 하기

--- FE/ghem/src/components/profile/info/ProfileInfo.tsx
+++ FE/ghem/src/components/profile/info/ProfileInfo.tsx
@@ -27,8 +27,10 @@ function ProfileInfo() {
     if (response) {
       const { user } = response;
 
+      console.log(user.steamId);
+
       if (user.nickname) setNickname(user.nickname);
-      if (user.steamId) setSteamId(user.steamId);
+      if (user.steamId && user.steamId !== "(NULL)") setSteamId(user.steamId);
       if (user.introduce) setIntroduce(user.introduce);
       setProfileImage(user.userProfile.substr(1, user.userProfile.length - 2));
 
