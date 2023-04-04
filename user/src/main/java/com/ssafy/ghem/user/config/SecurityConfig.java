c210faf6aab7509d21b3ff51ea0753d6edfd7fa1
이석원 <tjrdnjs67@naver.com>
2023-04-04 23:45:44 +0900

    🌚[be:fix] cors error

--- user/src/main/java/com/ssafy/ghem/user/config/SecurityConfig.java
+++ user/src/main/java/com/ssafy/ghem/user/config/SecurityConfig.java
@@ -1,48 +1,48 @@
-package com.ssafy.ghem.user.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().configurationSource(corsConfigurationSource()) // CORS 설정 추가
-                .and()
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .oauth2Login()
-                .defaultSuccessUrl("http://j8d107.p.ssafy.io/")
-                .and()
-                .logout()
-                .logoutSuccessUrl("http://j8d107.p.ssafy.io/"); // 프론트엔드 URL로 변경
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://j8d107.p.ssafy.io", "http://localhost")); // 프론트엔드 서버의 URL을 허용 목록에 추가
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("*"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-}
+//package com.ssafy.ghem.user.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().configurationSource(corsConfigurationSource()) // CORS 설정 추가
+//                .and()
+//                .authorizeRequests()
+//                .anyRequest().permitAll()
+//                .and()
+//                .oauth2Login()
+//                .defaultSuccessUrl("http://j8d107.p.ssafy.io/")
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("http://j8d107.p.ssafy.io/"); // 프론트엔드 URL로 변경
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://j8d107.p.ssafy.io", "http://localhost")); // 프론트엔드 서버의 URL을 허용 목록에 추가
+//        configuration.setAllowedMethods(Arrays.asList("*"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setExposedHeaders(Arrays.asList("*"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//
+//        return source;
+//    }
+//}
