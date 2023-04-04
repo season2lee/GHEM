8011cf099610f371ddb80e56038c2b29c24be041
이석원 <tjrdnjs67@naver.com>
2023-04-04 20:50:43 +0900

    🌚[be:fix] error fix

--- user/src/main/java/com/ssafy/ghem/user/config/SecurityConfig.java
+++ user/src/main/java/com/ssafy/ghem/user/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -33,9 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://j8d107.p.ssafy.io", "http://localhost:5173", "http://localhost:8081", "http://j8d107.p.ssafy.io:8081")); // 프론트엔드 서버의 URL을 허용 목록에 추가
+        configuration.setAllowedOrigins(Arrays.asList("http://j8d107.p.ssafy.io", "http://localhost")); // 프론트엔드 서버의 URL을 허용 목록에 추가
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true);
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
 
